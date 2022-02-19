c257f4d2908c0fa32a21e3dd46b6e3a9f11be03a
Tiratira (Daniel) <94789999+TiranexDev@users.noreply.github.com>
2022-02-19 23:03:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ function handle(err, stdout, stderr) {
     if(stdout) console.log(stdout)
 }
 
+let lets = []
 fs.readFile(process.cwd() + '/do.what', (err, content) => {
     if(!content) throw new Error('File must contain something')
     content = content.toString()
 
     content.split('\n').filter(line => line.trim() != "" && !line.startsWith("#")).forEach(l => {
-        if(l.startsWith("{") && l.includes("}")) {
+        if(l.startsWith("@")) {
+            let varu = l.split("=")[0].substring(1)
+            if(varu.endsWith(" ")) varu = varu.substring(0, varu.length - 1)
+            return lets.push({name:varu, equals: l.split("=")[1]})
+        } else if(l.startsWith("{") && l.includes("}")) {
             let splitter = l.split("}");
             let what = splitter[0].substring(1);
             let execute = splitter[1];
@@ -22,8 +27,14 @@ fs.readFile(process.cwd() + '/do.what', (err, content) => {
                     handle(err, stdout, stderr)
                 })
             }`)
-        } else exec(l, (err, stdout, stderr) => {
-            handle(err, stdout, stderr)
-        })
+        } else {
+            lets.forEach(variable => {
+                l = l.replaceAll(`@${variable.name}@`, variable.equals)
+            })
+
+            exec(l, (err, stdout, stderr) => {
+                handle(err, stdout, stderr)
+            })
+        }
     })
 })
