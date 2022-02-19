d7ae723e0c8db7634e60735cc31bd2c9676edbce
Tiratira (Daniel) <94789999+TiranexDev@users.noreply.github.com>
2022-02-19 23:14:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ fs.readFile(process.cwd() + '/do.what', (err, content) => {
     content = content.toString()
 
     content.split('\n').filter(line => line.trim() != "" && !line.startsWith("#")).forEach(l => {
+        lets.forEach(variable => {
+            l = l.replaceAll(`@${variable.name}@`, variable.equals)
+        })
+
         if(l.startsWith("@")) {
             let varu = l.split("=")[0].substring(1)
             if(varu.endsWith(" ")) varu = varu.substring(0, varu.length - 1)
             return lets.push({name:varu, equals: l.split("=")[1]})
+        } else if(l.startsWith("[")&& l.includes("if") && l.includes("]")) {
+            let splitter = l.split("]")
+            let expression = splitter[0].substring(1)
+            let execute = splitter[1].substring(1)
+
+            eval(`${expression} {
+                exec(${JSON.stringify(execute)}, (err, stdout, stderr) => {
+                    handle(err, stdout, stderr)
+                }) 
+            }`)
         } else if(l.startsWith("{") && l.includes("}")) {
             let splitter = l.split("}");
             let what = splitter[0].substring(1);
@@ -28,9 +42,6 @@ fs.readFile(process.cwd() + '/do.what', (err, content) => {
                 })
             }`)
         } else {
-            lets.forEach(variable => {
-                l = l.replaceAll(`@${variable.name}@`, variable.equals)
-            })
 
             exec(l, (err, stdout, stderr) => {
                 handle(err, stdout, stderr)
